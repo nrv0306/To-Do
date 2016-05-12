8044b5b9ea627d175adee1e66dd2fbb750c4f1ec
nrv0306 <nrv0306@gmail.com>
2016-05-12 10:35:36 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 angular.module("todoApp", [])
 .controller("TodoListController", function($scope) {
-	var todoList = this;
-	todoList.todoItems = [
-		{ name: "Go to school" },
-		{ name: "Eat lunch" },
-		{ name: "Do laundry" },
-		{ name: "Buy materials for project" }
-	]
+  var todoList = this;
+  todoList.todoItems = [
+    { name: "Go to school" },
+    { name: "Eat lunch" },
+    { name: "Do laundry" },
+    { name: "Buy materials for project" }
+  ]
+
+  todoList.addTodo = function() {
+    todoList.todoItems.push(
+      { name: todoList.todoText }
+    )
+    todoList.todoText = "";
+  };
 
 
-}); // end TodoListController
+});
