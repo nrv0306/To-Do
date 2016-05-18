5617566f5361a8917cf2a55a03148f907d6ab733
nrv0306 <nrv0306@gmail.com>
2016-05-18 10:33:52 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@ angular.module("todoApp", [])
 .controller("TodoListController", function($scope) {
   var todoList = this;
   todoList.todoItems = [
+    { name: "Get up", done: false },
     { name: "Go to school", done: false },
-    { name: "Eat lunch", done: false },
-    { name: "Do laundry", done: false },
-    { name: "Buy materials for project", done: false }
+    { name: "Go home", done: false },
+    { name: "Do homework and sleep", done: false }
+    
   ]
 
   todoList.addTodo = function() {
@@ -26,12 +27,11 @@ angular.module("todoApp", [])
 
   todoList.remaining = function() {
     var notCompletedCount = 0;
-    angular.forEach(todoList.TodoItems, function(todo) {
-      notCompletedCount += todo.done;
+    angular.forEach(todoList.todoItems, function(todo) {
+      notCompletedCount += todo.done ? 0 : 1;
     })
 
     return notCompletedCount
   }
-
 
 });
