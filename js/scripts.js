de4e45ea1997aa54e70957a742eb7cba5375c986
nrv0306 <nrv0306@gmail.com>
2016-05-20 10:32:58 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-angular.module("todoApp", [])
-.controller("TodoListController", function($scope) {
+angular.module("todoApp", []) // app created
+.controller("TodoListController", function($scope) { // controller created
   var todoList = this;
   todoList.todoItems = [
-    { name: "Get up", done: false },
-    { name: "Go to school", done: false },
-    { name: "Go home", done: false },
-    { name: "Do homework and sleep", done: false }
+    { name: "Get up", done: false }, // place holder set to be no done
+    { name: "Go to school", done: false }, // place holder set to be no done
+    { name: "Go home", done: false }, // place holder set to be no done
+    { name: "Do homework and sleep", done: false } // place holder set to be no done
     
   ]
 
-  todoList.addTodo = function() {
+  todoList.addTodo = function() { // add frunction created
     todoList.todoItems.push(
       { name: todoList.todoText }
     )
     todoList.todoText = "";
   };
 
-  todoList.remove = function() {
-    var oldTodos = todoList.todoItems;
+  todoList.remove = function() { // remove funtion created
+    var oldTodos = todoList.todoItems; 
     todoList.todoItems = [];
     angular.forEach(oldTodos, function(todo) {
       if(!todo.done) todoList.todoItems.push(todo);
@@ -25,10 +25,10 @@ angular.module("todoApp", [])
     todoList.todoItems
   };
 
-  todoList.remaining = function() {
-    var notCompletedCount = 0;
-    angular.forEach(todoList.todoItems, function(todo) {
-      notCompletedCount += todo.done ? 0 : 1;
+  todoList.remaining = function() { // remaining task funtion created
+    var notCompletedCount = 0; // start at 0
+    angular.forEach(todoList.todoItems, function(todo) { // each checked goes up
+      notCompletedCount += todo.done ? 0 : 1; // make it count backwards
     })
 
     return notCompletedCount
